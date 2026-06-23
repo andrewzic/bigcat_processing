5a108f4c43481f05840171f77a5e59757c78adf3
andrewzic <andrew.john.zic@gmail.com>
2026-06-23 17:10:58 +1000

    wsclean path improvement

--- scripts/cal_general.sh
+++ scripts/cal_general.sh
@@ -103,4 +103,4 @@ then
     casa 6 --nologger --nogui -c casa_import.py ${PROJ_DATA}/${target}.${freq}${ifext}.cal.uvfits
 fi
 
-wsclean -name ${target}.img -data-column DATA -save-source-list -multiscale -multiscale-scale-bias 0.8 -niter 25000 -pol i -weight briggs 1.5 -scale 1asec -size 2048 2048 -auto-threshold 3 -auto-mask 7.0 -join-channels -channels-out 8 -fit-spectral-pol 3 -minuv-l 1600 ${target}.${freq}${ifext}.cal.ms ; rm *000?-*.fits
+wsclean -name ${PROJ_DATA}/${target}.img -data-column DATA -save-source-list -multiscale -multiscale-scale-bias 0.8 -niter 25000 -pol i -weight briggs 1.5 -scale 1asec -size 2048 2048 -auto-threshold 3 -auto-mask 7.0 -join-channels -channels-out 8 -fit-spectral-pol 3 -minuv-l 1600 ${PROJ_DATA}/${target}.${freq}${ifext}.cal.ms ; rm ${PROJ_DATA}/*000?-*.fits
